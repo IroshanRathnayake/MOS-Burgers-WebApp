dc5103901e9dd7fb0a6cc3071cc91e559b935ee8
IroshanRathnayake <meet.iroshan@gmail.com>
2024-07-28 21:18:11 +0530

    Add modal to add a new customer

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
+//Order Management Section
+//Add Customer Modal in place-order.html
+
+const customer = {
+  firstName: "",
+  lastName: "",
+  occupation: "",
+  location: "",
+  email: "",
+  phoneNumber: "",
+  additionalInfo: "",
+};
+function addCustomer() {
+  console.log("Addustoemr");
+  customer.firstName = document.getElementById("modalFirstName").value;
+  customer.lastName = document.getElementById("modalLastName").value;
+  customer.occupation = document.getElementById("modalOccupation").value;
+  customer.email = document.getElementById("modalEmail").value;
+  customer.phoneNumber = document.getElementById("ModalPhoneNumber").value;
+  customer.location = document.getElementById("modalLocation").value;
+  customer.additionalInfo = document.getElementById(
+    "modalAdditionalInfomation"
+  ).value;
+  console.log(customer);
+  showAddCustomerModal();
+}
+
+function showAddCustomerModal() {
+  const myModal = new bootstrap.Modal("#modalAddCustomer");
+  myModal.show();
+}
+
+//Orders.html Table and Search functionality
 const search = document.querySelector(".input-group input"),
   table_rows = document.querySelectorAll("tbody tr"),
   table_headings = document.querySelectorAll("thead th");
@@ -144,3 +177,10 @@ const downloadFile = function (data, fileType, fileName = "") {
   a.click();
   a.remove();
 };
+
+//Place Order
+function btnPlaceOrderClicked() {
+  console.log("clicked");
+  const snipper = (document.getElementById("spinner").style.visibility =
+    "hidden");
+}
